6c1c317dfb2d28bb977bbf3c4ecb60406956ea18
AvirukBasak <avirukbasak@gmail.com>
2022-12-04 01:40:15 +0530

    updated libavl.h

--- include/libavl.h
+++ include/libavl.h
@@ -5,50 +5,79 @@
 #include <stdbool.h>
 
 /**
- * @brief AVL head type
+ * @brief AVL head type.
  * Needs to be manually allocated on the heap.
  */
 typedef struct AVL AVL;
 
 /**
- * @brief AVL tree node
+ * @brief AVL tree node.
  * Needs to be manually allocated on the heap.
  */
 typedef struct avlnode_t avlnode_t;
 
 /**
- * @brief AVL tree node data
+ * @brief AVL tree node data.
  * Needs to be manually allocated and linked with a node.
  */
 typedef struct avldata_t avldata_t;
 
 /**
- * @brief AVL head type
+ * @brief AVL head type.
  * Needs to be manually allocated on the heap.
  */
 struct AVL {
+    /**
+     * @brief Pointer to root node.
+     * Auto managed, DO NOT modify.
+     * @type avlnode_t*
+     */
     avlnode_t *__root;
 };
 
 /**
- * @brief AVL tree node type
+ * @brief AVL tree node type.
  * Needs to be manually allocated on the heap.
  */
 struct avlnode_t {
-    avldata_t *data;  // data
-    avlnode_t *__pr;  // parent
-    avlnode_t *__lc;  // left child
-    avlnode_t *__rc;  // right child
-    size_t     __bf;  // balance factor
+    /**
+     * @brief Data pointer. Needs to be manually linked to data.
+     */
+    avldata_t *data;
+    /**
+     * @brief Pointer to parent node.
+     * Auto managed, DO NOT modify.
+     * @type avlnode_t*
+     */
+    avlnode_t *__pr;
+    /**
+     * @brief Pointer to left child node.
+     * Auto managed, DO NOT modify.
+     * @type avlnode_t*
+     */
+    avlnode_t *__lc;
+    /**
+     * @brief Pointer to right child node.
+     * Auto managed, DO NOT modify.
+     * @type avlnode_t*
+     */
+    avlnode_t *__rc;
+    /**
+     * @brief Node balance factor.
+     * Auto managed, DO NOT modify.
+     * @type size_t
+     */
+    size_t __bf;
 };
 
 /**
  * @brief Callback function, compares data to data b/w nodes.
  * @param avldata_t* AVL tree node data
  * @param avldata_t* AVL tree node data
- * @return int If 0, match found
- * @return int If -ve i.e. decrease, libavl goes to left subtree
- * @return int If +ve i.e. increase, libavl goes to right subtree
+ * @return int.
+ * @return If 0, match found.
+ * @return If -ve i.e. decrease, libavl goes to left subtree.
+ * @return If +ve i.e. increase, libavl goes to right subtree.
  */
 typedef int (*avl_compare_t)(avldata_t*, avldata_t*);
 
@@ -56,9 +85,10 @@ typedef int (*avl_compare_t)(avldata_t*, avldata_t*);
  * @brief Callback function, compares a key to data b/w nodes.
  * @param void* Pointer to key which is to be searched
  * @param avldata_t* AVL tree node data
- * @return int If 0, match found
- * @return int If -ve i.e. decrease, libavl goes to left subtree
- * @return int If +ve i.e. increase, libavl goes to right subtree
+ * @return int.
+ * @return If 0, match found.
+ * @return If -ve i.e. decrease, libavl goes to left subtree.
+ * @return If +ve i.e. increase, libavl goes to right subtree.
  */
 typedef int (*avl_keycompare_t)(void*, avldata_t*);
 
